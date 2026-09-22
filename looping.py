d898b039e22942b93b16b2a0a2856a99f250721e
madebyyeaa <yoramestomihiarijaya@gmail.com>
2026-09-22 17:41:04 +0700

    belajar menggunakan looping

---
+++ looping.py
@@ -0,0 +1,18 @@
+# Looping
+for i in range(3):
+    print('Hi!');
+
+# for loop sering digunakan dalam list
+tools: list[str] = ['laptop','keyboard','mouse','monitor','cpu'];
+
+for tool in tools:
+   print(f'This is a {tool}');
+
+# while loop
+while True:
+    print('Hello');
+
+i: int = 0
+while i < 7:
+    print(i)
+    i += 1
