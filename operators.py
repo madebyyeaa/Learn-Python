276eb24cbc86a4b07d28685301bddb7445fa716a
madebyyeaa <yoramestomihiarijaya@gmail.com>
2026-09-23 22:12:53 +0700

    belajar mengenal operator perbandingan

---
+++ operators.py
@@ -0,0 +1,12 @@
+a: int = 1;
+b: int = 2;
+
+# operator perbandingan
+print(a > b);  # membandingkan apakah nilai a lebih besar dari b  
+print(a < b);  # membandingkan apakah nilai a lebih kecil dari b  
+print(a >= b); # membandingkan apakah nilai a lebih besar atau sama dengan b
+print(a <= b); # membandingkan apakah nilai a lebih kecil atau sama dengan b
+print(a == b); # membandingkan apakah nilai a dan b sama 
+print(a != b); # membandingkan apakah nilai a dan b tidak sama
+
+
