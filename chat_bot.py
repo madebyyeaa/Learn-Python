5f2d7c3b8809e51ce130013121cb16a44478120a
madebyyyea <yoramestomihiarijaya@gmail.com>
2026-09-26 21:47:03 +0700

    second commit rebase file

--- chat_bot.py
+++ chat_bot.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 bot_name: str = 'Yoram';
 print(f'Halo saya {bot_name}! apa yang dapat saya bantu?');
 
@@ -10,3 +11,21 @@ while True:
         print(f'{bot_name} Bye! besok ketemu lagi ya teman.');
     else:
         print(f'{bot_name} maaf saya tidak mengerti ketik hai, halo, bye, sampa jumpa');
+=======
+user_bot: str = 'Bang IT'
+print(f'{user_bot} : Woi sopan sikit abang IT ni, kendala apa?')
+
+while True:
+    user_input: str = input('You: ').lower()
+    
+    if user_input in ['help bang']:
+        print(f'{user_bot}: Ada kendala apa?')
+    elif user_input in ['mohon dibantu bang']:
+        print(f'{user_bot}: Ada kendala apa cuk?!!')
+    elif user_input in ['tdk ada jaringan bang', 'jaringan hilang']:
+        print(f'{user_bot}: Oke down ya, restart dulu modemnya 15 menit')
+    elif user_input in ['jaringan hilang']:
+        print(f'{user_bot}: Sudah restart modem belum? restart dulu kalau belum!')
+    else:
+        print(f'Masukan kata kunci dengan sesuai panduan di atas coba lagi')
+>>>>>>> 2d313e7 (belajar membuat dan mengimport modul)
---
+++ readme.txt
@@ -0,0 +1,1 @@
+Yoram E Arijaya
